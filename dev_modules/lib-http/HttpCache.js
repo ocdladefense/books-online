f337f2b936480ed17cc1a15f0e7062bccc6b17b7
Ubuntu <ubuntu@ip-172-31-58-171.us-west-2.compute.internal>
2024-07-24 16:14:56 -0700

    Subsume legacy version of @ocdla/lib-http (@ocdladefense/lib-http) into this repository.

---
+++ dev_modules/lib-http/HttpCache.js
@@ -0,0 +1,23 @@
+export { HttpCache }
+
+
+class HttpCache {
+
+    static cache = {};
+
+    static add(req, resp) {
+
+        let key = req.url;
+        //let lang = req.getHeaders("accept-language");
+        //key = key + "-" + lang;
+        HttpCache.cache[key] = resp;
+    }
+
+    static get(req) {
+        let key = req.url;
+
+        return HttpCache.cache[key] ? HttpCache.cache[key].clone() : null;
+    }
+
+
+}
