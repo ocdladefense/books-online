d8badd68d969aeea3ba157da84f0a6c142de6d82
SullivanKE <Katelyn135@gmail.com>
2024-07-26 12:20:47 -0700

    Website loads properly; Added some TODOs for myself; Cleaned up scripts and removed duplicate code; Requires further cleaning.

--- src/js/BooksOnlineController.js
+++ src/js/BooksOnlineController.js
@@ -5,9 +5,9 @@ import { WebcOrs } from "/node_modules/@ocdladefense/webc-ors/src/WebcOrs.js";
 import { WebcOar } from "/node_modules/@ocdladefense/webc-oar/src/WebcOar.js";
 import "/node_modules/@ocdladefense/html/html.js";
 import domReady from "/node_modules/@ocdladefense/web/src/web.js";
-import init from "/js/init.js";
+import init from "/js/init.js"; // TODO: This wasn't being referenced. Find out what it does.
 import { DomDocument } from "/node_modules/@ocdladefense/dom/src/DomDocument.js";
-import { formatReferences, doRefs } from "./js/citations.js";
+import { formatReferences, doRefs } from "./citations.js";
 
 
 
@@ -26,11 +26,6 @@ export default class BooksOnlineController {
         window.modal = this.modal;
         OregonLegislatureNetwork.setUrl("https://appdev.ocdla.org/books-online/index.php");
     
-    
-
-
-        
-
 
         // customElements.define("word-count", WordCount, { extends: "p" });
         customElements.define("webc-ors", WebcOrs);
@@ -44,7 +39,9 @@ export default class BooksOnlineController {
         domReady(init);
 
 
-
+// TODO: Format the disparate scripts and clean them up. Move the inline functions into methods as needed.
+// TODO: BooksOnlineController is named poorly now. We know it's a controller. BooksOnline.js is fine. Clean up references to it and rename it.
+// TODO: Follow the scripts and see where they lead. Familiarize myself with their structure and functions.
 
     //<!-- Process all citations in this document. List the citations as HTML links.  These links can be selected by the customer to navigate to where the source is referenced in the chapter.  -->
 
@@ -117,87 +114,6 @@ export default class BooksOnlineController {
     customElements.define("webc-ors", WebcOrs);
     customElements.define("webc-oar", WebcOar);
 
-    // Next script
-    
-
-    const controller = new Controller();
-    domReady(() => document.addEventListener("click", controller));
-    domReady(() => controller.convert(".chapter"));
-    domReady(init);
-
-
-//<!-- Process all citations in this document. List the citations as HTML links.  These links can be selected by the customer to navigate to where the source is referenced in the chapter.  -->
-
-    
-
-    let refContainer = document.querySelector("#all-refs");
-    let citations = document.querySelectorAll(".cite");
-    let refs = document.querySelectorAll("[references], .cite");
-
-    domReady(function () {
-        formatReferences(citations);
-        doRefs(refs, refContainer);
-    });
-
-
-    window.addEventListener("hashchange", function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-
-        let newId = e.newURL.split("#")[1];
-        let newElem = document.getElementById(newId);
-        console.log(newId);
-
-        newElem.scrollIntoView({
-            behavior: "smooth",
-            block: "center",
-            inline: "nearest",
-        }); //({top: (rect.y + offset),behavior:"smooth"});
-    });
-
-//<!-- Display table of contents (TOC) content and modal. This should display other chapters in the current publication. -->
-
-    
-
-    window.loadToc = loadToc;
-    function loadToc() {
-        let config = {
-            style: "display:block; width:auto; vertical-align:top; overflow-x: hidden; overflow-y: auto; height:60vh; padding: 8px;"
-        };
-        let modal = new Modal(config);
-        modal.show();
-        fetch("/sites/pubs.ocdla.org/books/" + window.book + "/toc.tpl.php").then((resp) => {
-            return resp.text();
-        })
-            .then((html) => {
-                modal.render(html);
-            });
-    }
-
-
-//<!-- Setup the chapter ouline and display it inside of the div.outline-content element. Currently not displaying so we need to figure out what is going wrong. -->
-
-    
-    window.DomDocument = DomDocument;
-
-    domReady(init);
-
-    // Use these headings to create an on-the-fly outline of the document.
-    function init() {
-        let doc = new DomDocument();
-        let nodes = doc.outline("h2"); // h1, h2, h3
-        nodes.forEach((node) => document.querySelector(".outline-content").appendChild(node));
-    }
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     }
 
