3a46f33871e1d3018a415d74cc40e734a83dd215
José Bernal <jbernal.web.dev@gmail.com>
2024-08-13 11:39:19 -0700

    Rename Chapter to be a TOC entry.

--- dev_modules/@ocdla/table-of-contents/TableOfContents.js
+++ dev_modules/@ocdla/table-of-contents/TableOfContents.js
@@ -1,86 +1,47 @@
-import Chapter from "./Chapter";
+import Entry from "./Entry";
+
+
+
 export default class TableOfContents {
-    #chapters = new Array();
-    constructor(chapters) {
-        this.#chapters = chapters;
-    }       
-    static fromXml(xml) {
-        const items = [...xml.querySelectorAll("section, chapter")];
-        const chapters = items.map((item) => {
-            
-            const nodeName = item.nodeName;
-            const titleCase = nodeName.charAt(0).toUpperCase() + nodeName.substring(1).toLowerCase();
-            const heading = titleCase + ' ' + item.id.split('-')[1];
-            return new Chapter(item.id, item.attributes.name.textContent, heading)
+  #entries = new Array();
+
+  constructor(entries) {
+    this.#entries = entries;
+  }
+
+  static fromXml(doc) {
+    const items = [...doc.querySelectorAll("part, chapter, appendix")];
+    const entries = items.map((item) => {
+      return new Entry(
+        item.getAttribute("name"),
+        item.getAttribute("label"),
+        item.id
+      );
     });
-        return new TableOfContents(chapters);
-    }
+    return new TableOfContents(entries);
+  }
 
-    getChapters() {
-        return this.#chapters;
-    }
+  getEntries() {
+    return this.#entries;
+  }
 
-    toNodeTree() {
-        // Make our table of contents root element
-        const root = document.createElement("div");
-        root.setAttribute("class", "toc-content");
+  toNodeTree() {
+    // Make our table of contents root element
+    const root = document.createElement("div");
+    root.setAttribute("class", "toc-content");
 
-        // Loop through our chapters and add them to the table of contents
-        this.#chapters.forEach((chapter) => {
+    let entries = this.#entries.map(entry => {
+        return entry.toNode();
+    });
 
-            // Make our table of contents item
-            
-            //div.setAttribute("class", "nav-item");
+    // Loop through our chapters and add them to the table of contents
+    entries.forEach((node) => {
+      // Make our table of contents item
 
-            
+      root.appendChild(node);
+    });
 
-            // Make our table of contents link from our ID.
-            // This is the entire item as well, since we want it all to be clickable
-            const href = '/' + chapter.getId().replace("-", "/");
-            const a = document.createElement("a");
-            a.setAttribute("href", href);
-            a.setAttribute("title", ' - ' + chapter.getName());
-            a.setAttribute("class", "nav-item");
-            a.setAttribute("data-book", chapter.getBook());
-            a.setAttribute("data-chapter", chapter.getChapter());
-            
+    return root;
+  }
 
-            // Get the name of our TOC item
-            // Example: Crime Seriousness Rankings
-            const div = document.createElement("div");
-            div.setAttribute("class", "chapter-name");
-            div.innerHTML = chapter.getName();
-
-
-            // Get the label of our TOC item
-            // Example: Chapter 1
-            const span = document.createElement("span");
-            span.setAttribute("class", "label");
-            span.innerHTML = chapter.getHeading();
-
-            // We don't want sections to have labels
-            if (chapter.isChapter())
-                a.appendChild(span);
-
-            a.appendChild(div);
-            root.appendChild(a);
-        });
-
-        return root;
-    }
-
-    toHtml() {
-        let doc = new Document();
-        let wordSection = doc.createElement("div");
-        wordSection.setAttribute("class", "WordSection1");
-        let nodeTree = this.toNodeTree();
-
-        wordSection.appendChild(nodeTree);
-        doc.appendChild(wordSection);
-        
-        const serializer = new XMLSerializer();
-        const subset = doc.querySelector(".WordSection1");
-        return serializer.serializeToString(subset);
-
-    }
 }
