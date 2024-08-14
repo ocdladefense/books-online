77e0ffd40851e67f0a8ed8f8bf1afb0890660d16
SullivanKE <Katelyn135@gmail.com>
2024-08-14 13:23:06 -0700

    Set up Trung's workflow into books online

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,15 @@
-import BooksOnlineController from './BooksOnlineController.js';
+import BooksOnlineController from "./BooksOnlineController.js";
 
-import '/node_modules/@ocdladefense/modal/src/css/loading.css';
-import '/node_modules/@ocdladefense/modal/src/css/modal.css';
-import '/node_modules/@ocdladefense/modal/src/css/inline-modal.css';
-import '/themes/books-online/css/main.css';
-import '/themes/books-online/css/citations.css';
-import '/themes/books-online/css/headings.css';
-import '/themes/books-online/css/toc.css';
-import '/themes/books-online/css/modal.css';
-import '/themes/books-online/css/tools.css';
-import '/themes/books-online/css/desktop.css'; //'(min-width:767px)' 
-
+import "/node_modules/@ocdladefense/modal/src/css/loading.css";
+import "/node_modules/@ocdladefense/modal/src/css/modal.css";
+import "/node_modules/@ocdladefense/modal/src/css/inline-modal.css";
+import "/themes/books-online/css/main.css";
+import "/themes/books-online/css/citations.css";
+import "/themes/books-online/css/headings.css";
+import "/themes/books-online/css/toc.css";
+import "/themes/books-online/css/modal.css";
+import "/themes/books-online/css/tools.css";
+import "/themes/books-online/css/desktop.css"; //'(min-width:767px)'
 
 import HttpClient from "@ocdla/lib-http/HttpClient.js";
 
@@ -18,13 +17,11 @@ import HttpClient from "@ocdla/lib-http/HttpClient.js";
 // import { OarApiMock } from "./mock/OarApiMock.js";
 import { BonMock } from "./mock/BonMock.js";
 
-if (USEMOCK) {
-    //HttpClient.register("https://appdev.ocdla.org/", new OrsApiMock());
-    //HttpClient.register("https://appdev.ocdla.org/", new OarApiMock());
-    HttpClient.register("https://pubs.ocdla.org/", new BonMock());
+console.log(USE_MOCK);
+if (USE_MOCK) {
+  //HttpClient.register("https://appdev.ocdla.org/", new OrsApiMock());
+  //HttpClient.register("https://appdev.ocdla.org/", new OarApiMock());
+  HttpClient.register("https://pubs.ocdla.org/", new BonMock());
 }
-
-
-
 
 let controller = new BooksOnlineController();
