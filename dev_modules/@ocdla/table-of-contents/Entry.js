8c6233826b386594f42b01ca7acd710c13ba33cc
SullivanKE <Katelyn135@gmail.com>
2024-08-14 11:25:41 -0700

    Further removed chapter nomenclature from TableOfContents with unit.

--- dev_modules/@ocdla/table-of-contents/Entry.js
+++ dev_modules/@ocdla/table-of-contents/Entry.js
@@ -29,45 +29,40 @@ export default class Entry {
     return this.#id.split("-")[0];
   }
 
-  getChapter() {
+  getUnit() {
     return this.#id.split("-")[1];
   }
 
-  
-
   toNode() {
+    // Make our table of contents link from our ID.
+    // This is the entire item as well, since we want it all to be clickable
+    const href = "/" + this.getId().replace("-", "/");
+    const a = document.createElement("a");
+    a.setAttribute("id", this.getId());
+    a.setAttribute("href", href);
+    a.setAttribute("title", " - " + this.getName());
+    a.setAttribute("class", "nav-item");
+    a.setAttribute("data-book", this.getBook());
+    a.setAttribute("data-unit", this.getUnit());
 
-      // Make our table of contents link from our ID.
-      // This is the entire item as well, since we want it all to be clickable
-      const href = "/" + this.getId().replace("-", "/");
-      const a = document.createElement("a");
-      a.setAttribute("id", this.getId());
-      a.setAttribute("href", href);
-      a.setAttribute("title", " - " + this.getName());
-      a.setAttribute("class", "nav-item");
-      a.setAttribute("data-book", this.getBook());
-      a.setAttribute("data-chapter", this.getChapter());
+    // Get the name of our TOC item
+    // Example: Crime Seriousness Rankings
+    const div = document.createElement("div");
+    div.setAttribute("class", "chapter-name");
+    div.innerHTML = this.getName();
 
-      // Get the name of our TOC item
-      // Example: Crime Seriousness Rankings
-      const div = document.createElement("div");
-      div.setAttribute("class", "chapter-name");
-      div.innerHTML = this.getName();
+    // Get the label of our TOC item
+    // Example: Chapter 1
+    const span = document.createElement("span");
+    span.setAttribute("class", "label");
+    span.innerHTML = this.getHeading();
 
-      // Get the label of our TOC item
-      // Example: Chapter 1
-      const span = document.createElement("span");
-      span.setAttribute("class", "label");
-      span.innerHTML = this.getHeading();
+    // We don't want sections to have labels
+    if (this.isChapter()) {
+      a.appendChild(span);
+    }
 
-      // We don't want sections to have labels
-      if (this.isChapter()) {
-        a.appendChild(span);
-      }
-
-      a.appendChild(div);
-
-
+    a.appendChild(div);
 
     return a;
   }
