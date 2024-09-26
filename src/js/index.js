30a8aca062bc6c44e4faa91024d92fce0647fa1a
José Bernal <jbernal.web.dev@gmail.com>
2024-09-26 16:18:12 -0700

    Add some webc-ors divs and test code to download chapters.

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,8 @@ import "/themes/books-online/css/modal.css";
 import "/themes/books-online/css/tools.css";
 import "/themes/books-online/css/desktop.css";
 import HttpClient from "@ocdla/lib-http/HttpClient.js";
+import WebcOrs from "@ocdladefense/webc-ors/src/WebcOrs.js";
+import {parseChapterAndSection, parseSubsections, parseReferences, toSelectors} from "@ocdladefense/ors/src/ReferenceParser.js";
 
 import { BonMock } from "./mock/BonMock.js";
 
@@ -16,4 +18,19 @@ if (USE_MOCK) {
   HttpClient.register("https://pubs.ocdla.org/", new BonMock());
 }
 
+window.parseChapterAndSection = parseChapterAndSection;
+window.parseSubsections = parseSubsections;
+window.parseReferences = parseReferences;
+window.toSelectors = toSelectors;
+window.WebcOrs = WebcOrs;
+
+function loadOrs(ref) {
+  let chapter = WebcOrs.loadChapter(182);
+  chapter.then((chapter) => {
+    console.log(chapter);
+    chapter.download();
+  });
+}
+
+// loadOrs(182);
 let controller = new BooksOnlineController();
