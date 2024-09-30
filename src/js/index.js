a7761cb0f2085a3001d249388d8ef237ef3e0deb
José Bernal <jbernal.web.dev@gmail.com>
2024-09-30 09:57:45 -0700

    Pin @ocdladefense/ors and @ocdladefense/webc-ors to new commits.

--- src/js/index.js
+++ src/js/index.js
@@ -11,7 +11,7 @@ import "/themes/books-online/css/desktop.css";
 import HttpClient from "@ocdla/lib-http/HttpClient.js";
 import WebcOrs from "@ocdladefense/webc-ors/src/WebcOrs.js";
 import WebcOar from "@ocdladefense/webc-oar/src/WebcOar.js";
-import {parseChapterAndSection, parseSubsections, parseReferences, toSelectors} from "@ocdladefense/ors/src/ReferenceParser.js";
+import {ReferenceParser} from "@ocdladefense/ors/src/ReferenceParser.js";
 
 import { BonMock } from "./mock/BonMock.js";
 
@@ -19,32 +19,43 @@ if (USE_MOCK) {
   HttpClient.register("https://pubs.ocdla.org/", new BonMock());
 }
 
+/*
 window.parseChapterAndSection = parseChapterAndSection;
 window.parseSubsections = parseSubsections;
 window.parseReferences = parseReferences;
 window.toSelectors = toSelectors;
 window.WebcOrs = WebcOrs;
+*/
 
 
-
+// https://appdev.ocdla.org/books-online/index.php?chapter=182
+// https://www.law.cornell.edu/regulations/oregon/OAR-863-010-0610
 function loadOrs(ref) {
   
-  // Translate the reference into selectors.
-  let selectors = toSelectors(ref);
-  console.log("For "+ref+" selectors are: ", selectors);
+  // Translate the reference into matrixes.
+  // Each target document (in this case an OrsChapter object) should know how to translate a matrix into selectors
+  // valid for that document type.
+  let matrixes = ReferenceParser.toMatrix(ref);
+
+
+  console.log("For "+ref+" matrixes are: ", matrixes);
   // 182.515 Definitions for ORS 182.515 and 182.525.
 
-  let load = WebcOrs.loadChapter(182);
+  let load = WebcOrs.loadChapter(138);
   load.then((chapter) => {
     // console.log(chapter);
     // chapter.download();
     // Note: had to truncate the 182 off.
-    let range = chapter.getRange([515,2], [515,4]);
-    console.log(range.cloneContents());
+    let selectors = chapter.toSelectors(matrixes);
+    console.log("SELECTORS: ", selectors);
+    let nodes = chapter.getNodes(selectors);
+
+    console.log(nodes);
   });
 }
 
 
 
 // loadOrs("182.515(5),(2)-(3)");
+// loadOrs("138.005(5)(a)-(b)");
 let controller = new BooksOnlineController();
