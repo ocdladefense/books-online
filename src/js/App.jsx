5ea9b266038c5a180fc7ea90101a2be4149be906
SullivanKE <KatelynSullivan135@gmail.com>
2025-01-06 18:36:06 -0800

    Moved initial render out from the controller and into App.jsx.

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -4,116 +4,49 @@
 import { vNode } from "@ocdla/view";
 import Navbar from "@ocdla/global-components/src/Navbar";
 import Breadcrumbs from "@ocdla/global-components/src/Breadcrumbs";
-import Sidebar from "@ocdla/global-components/src/Sidebar";
-import ORS_Section_Link from "./components/ORS_Section_Link";
-import Sidebar_Item from "@ocdla/global-components/src/Sidebar_Item";
-import Body from "@ocdla/global-components/src/Body";
 import Footer from "@ocdla/global-components/src/Footer";
-/* eslint-enable */
-import Books_Online_Breadcrumbs_Items from "../data/json/books-online/breadcrumbs/items.json";
-import Books_Online_Sidebar_Left_Items from "../data/json/books-online/sidebar_left/items.json";
-import Books_Online_Sidebar_Right_Items from "../data/json/books-online/sidebar_right/items.json";
 
-export default function App({
-  view,
-  currentAppType,
-  headerPinned,
-  currentVolume,
-  currentTitle,
-  currentChapter,
-  currentSection,
-  items_breadcrumbs_ors_viewer,
-  items_sidebar_left_ors_viewer,
-  items_sidebar_left_books_online,
-  html_body_ors_viewer,
-  items_sidebar_right_ors_viewer,
-}) {
-  const appTypeIndicators = currentAppType ? "📚" : "🔍";
-  const appTypeString = currentAppType ? "books-online" : "ors-viewer";
-
-  // console.log(items_sidebar_left_books_online);
+export default function App({ }) {
 
   return (
     <>
       <div
         // Preserve whitespace at end of top-0
         // prettier-ignore
-        class={`${headerPinned === 'pinned' ? 'fixed ' : 'absolute '}right-0 z-10 flex w-max gap-2 bg-white p-4 lg:left-0 lg:p-2`}
-      >
-        <button
-          class="select-none font-bold"
-          onclick={() => {
-            currentAppType = !currentAppType;
-
-            view.render(
-              <App
-                headerPinned={headerPinned}
-                currentVolume={currentVolume}
-                currentTitle={currentTitle}
-                currentChapter={currentChapter}
-                currentSection={currentSection}
-                items_breadcrumbs_ors_viewer={items_breadcrumbs_ors_viewer}
-                items_sidebar_left_ors_viewer={items_sidebar_left_ors_viewer}
-                items_sidebar_left_books_online={
-                  items_sidebar_left_books_online
-                }
-                html_body_ors_viewer={html_body_ors_viewer}
-                items_sidebar_right_ors_viewer={items_sidebar_right_ors_viewer}
-              />
-            );
-          }}
-        >
-          {appTypeIndicators} | {appTypeString}
-        </button>
-      </div>
-      <header
-        // Preserve whitespace at end of top-0
-        // prettier-ignore
-        class={`${headerPinned === 'pinned' ? 'sticky top-0 ' : ''}container mx-auto flex w-full flex-col bg-white lg:h-32`}
-      >
+        class='fixed right-0 z-10 flex w-max gap-2 bg-white p-4 lg:left-0 lg:p-2'
+      ></div>
+      <header class="container mx-auto flex w-full flex-col bg-white lg:h-32 top-of-page">
         <Navbar />
-        <Breadcrumbs
-          items={
-            currentAppType
-              ? Books_Online_Breadcrumbs_Items
-              : currentAppType === false
-              ? items_breadcrumbs_ors_viewer
-              : []
-          }
-        />
+
       </header>
+
+
       {/* <Main cols='3' /> */}
       <div class="container mx-auto border-x">
+        <div id="breadcrumbs" class="sticky top-0 z-5 bg-white lg:static lg:top-auto lg:z-auto lg:bg-transparent">
+          <Breadcrumbs items={[]} />
+        </div>
+        <button
+          onclick={() => {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }}
+          class="fixed bottom-0 right-0 z-20 rounded-lg p-4 bg-black text-white"
+        >
+          Top
+        </button>
         {/* <div class='flex flex-col lg:flex-row'> */}
-        <div class="lg:grid lg:grid-cols-6">
-          {/* ? Ors_Viewer_Sidebar_Left_Items */}
-          <Sidebar>
-            {/* eslint-disable indent */}
-            {currentAppType
-              ? Books_Online_Sidebar_Left_Items.map((item) => (
-                  <ORS_Section_Link {...item} />
-                ))
-              : items_sidebar_left_ors_viewer.map((item) => (
-                  <ORS_Section_Link {...item} />
-                ))}
-            {/* eslint-enable */}
-          </Sidebar>
-          <Body
-            view={view}
-            type={appTypeString}
-            html_body_ors_viewer={html_body_ors_viewer}
-          />
-          <Sidebar>
-            {/* eslint-disable indent */}
-            {currentAppType
-              ? Books_Online_Sidebar_Right_Items.map((item) => (
-                  <Sidebar_Item {...item} />
-                ))
-              : items_sidebar_right_ors_viewer.map((item) => (
-                  <Sidebar_Item {...item} />
-                ))}
-            {/* eslint-enable */}
-          </Sidebar>
+        <div class="lg:grid lg:grid-cols-6" id="touch-area">
+          <div id="toc" class="fixed top-0 right-[100%] z-10 h-screen shadow-2xl lg:shadow-none lg:h-auto lg:static lg:top-auto lg:right-auto bg-white"></div>
+          <div
+            id="document"
+            class="flex w-full flex-col gap-4 p-4 lg:col-span-4 lg:col-start-2 lg:me-auto lg:border-x lg:p-8"
+          >
+            <div
+              id="body"
+              class="flex flex-col gap-4 subpixel-antialiased overflow-wrap break-words"
+            ></div>
+          </div>
+          <div id="outline" class="fixed top-0 left-[100%] z-10 h-screen shadow-2xl lg:shadow-none lg:h-auto lg:static lg:top-auto lg:left-auto bg-white"></div>
         </div>
       </div>
       <Footer
--- src/js/BooksOnlineController.js
+++ src/js/BooksOnlineController.js
@@ -8,13 +8,12 @@ import "@ocdladefense/html/html.js";
 import HttpClient from "@ocdla/lib-http/HttpClient.js";
 import Outline from "@ocdla/outline";
 import TableOfContents from "@ocdla/table-of-contents";
-import Footer from "@ocdla/global-components/src/Footer.jsx";
-import Navbar from "@ocdla/global-components/src/Navbar.jsx";
 import Breadcrumbs from "@ocdla/global-components/src/Breadcrumbs.jsx";
 import Sidebar from "@ocdla/global-components/src/Sidebar.jsx";
 import OutlineSidebar from "@ocdla/global-components/src/Outline.jsx";
 import Sidebar_Item_Left from "@ocdla/global-components/src/SidebarItemLeft.jsx";
 import Url from "@ocdla/lib-http/Url";
+import App from "./App.jsx";
 
 // Is this the best syntax for these imports?
 import Hammer from "hammerjs/hammer.js";
@@ -34,54 +33,8 @@ export default class BooksOnlineController {
     // Create the base view using jsx.
     const body = document.querySelector("body");
     const root = View.createRoot(body);
-    root.render(
-      <>
-        <div
-          // Preserve whitespace at end of top-0
-          // prettier-ignore
-          class='fixed right-0 z-10 flex w-max gap-2 bg-white p-4 lg:left-0 lg:p-2'
-        ></div>
-        <header class="container mx-auto flex w-full flex-col bg-white lg:h-32 top-of-page">
-          <Navbar />
-          
-        </header>
+    root.render(<App />);
 
-
-        {/* <Main cols='3' /> */}
-        <div class="container mx-auto border-x">
-        <div id="breadcrumbs" class="sticky top-0 z-5 bg-white lg:static lg:top-auto lg:z-auto lg:bg-transparent">
-            <Breadcrumbs items={[]} />
-          </div>
-          <button
-            onclick={() => {
-              window.scrollTo({ top: 0, behavior: "smooth" });
-            }}
-            class="fixed bottom-0 right-0 z-20 rounded-lg p-4 bg-black text-white"
-          >
-            Top
-          </button>
-          {/* <div class='flex flex-col lg:flex-row'> */}
-          <div class="lg:grid lg:grid-cols-6" id="touch-area">
-            <div id="toc" class="fixed top-0 right-[100%] z-10 h-screen shadow-2xl lg:shadow-none lg:h-auto lg:static lg:top-auto lg:right-auto bg-white"></div>
-            <div
-              id="document"
-              class="flex w-full flex-col gap-4 p-4 lg:col-span-4 lg:col-start-2 lg:me-auto lg:border-x lg:p-8"
-            >
-              <div
-                id="body"
-                class="flex flex-col gap-4 subpixel-antialiased overflow-wrap break-words"
-              ></div>
-            </div>
-            <div id="outline" class="fixed top-0 left-[100%] z-10 h-screen shadow-2xl lg:shadow-none lg:h-auto lg:static lg:top-auto lg:left-auto bg-white"></div>
-          </div>
-        </div>
-        <Footer
-          showFacebook={true}
-          showTwitter={true}
-          useGoogleMapsIFrame={true}
-        />
-      </>
-    );
 
 
     // This uses the Hammer.js library to detect panning on the page.
