0684b920d761c976d0f34ffce7911e6d9ca5654a
SullivanKE <KatelynSullivan135@gmail.com>
2025-01-09 20:24:26 -0800

    Reintroduced table of contents change from Mobile-UI branch. Breadcrumbs now render in the app.jsx using react hooks.

--- dev_modules/@local/global-components/src/Breadcrumbs.jsx
+++ dev_modules/@local/global-components/src/Breadcrumbs.jsx
@@ -5,25 +5,25 @@ import BreadcrumbItem from './BreadcrumbItem';
 import BreadcrumbDropdown from './BreadcrumbDropdown';
 /* eslint-enable */
 
-export default function Breadcrumbs({ crumbs = [] }) {
+export default function Breadcrumbs({ items = [] }) {
     return (
         <section class='flex items-center border border-t-0 p-4 capitalize text-black lg:h-16'>
             <ul class='flex flex-wrap items-center whitespace-pre'>
-                {crumbs.map((crumb, i) => {
+                {items.map((item, i) => {
                     const seperatorString =
-                        i !== crumbs.length - 1 ? ' / ' : ' ';
+                        i !== items.length - 1 ? ' / ' : ' ';
 
-                    if (crumb.entries)
+                    if (item.entries)
                         return (
                             <>
-                                <BreadcrumbDropdown {...crumb} />
+                                <BreadcrumbDropdown {...item} />
                                 {seperatorString}
                             </>
                         );
-                    console.log(crumb);
+                    console.log(item);
                     return (
                         <>
-                            <BreadcrumbItem {...crumb} />
+                            <BreadcrumbItem {...item} />
                             {seperatorString}
                         </>
                     );
--- src/js/App.jsx
+++ src/js/App.jsx
@@ -6,6 +6,7 @@ import HttpClient from "@ocdla/lib-http/HttpClient.js";
 import Navbar from "@ocdla/global-components/src/Navbar";
 import Breadcrumbs from "@ocdla/global-components/src/Breadcrumbs";
 import Footer from "@ocdla/global-components/src/Footer";
+import TableOfContents from "@ocdla/table-of-contents";
 
 
 const USE_HAMMER = false;
@@ -27,6 +28,16 @@ async function fetchChapter(book, chapter) {
   const html = await resp.text();
 
   return html;
+}
+
+function getChapterList(index, book) {
+  const elems = index.querySelectorAll(`book[shortName="${book}"] > * > :is(part, chapter, appendix)`);
+  return TableOfContents.fromXml(elems);
+}
+
+function getBookList(index) {
+  const elems = index.querySelectorAll('book');
+  return TableOfContents.fromXml(elems);
 }
 
 
@@ -58,24 +69,83 @@ async function renderContent(book, unit) {
   });
 }
 
+async function getIndex() {
+  let client = new HttpClient();
+  let resp = await client.send(new Request("https://pubs.ocdla.org/index"));
+  let xml = await resp.text();
+  const parser = new DOMParser();
+  return parser.parseFromString(xml, "application/xml");
+}
+
 
 
 export default function App({ }) {
 
+  const [index, setIndex] = useState(null);
   const [html, setHtml] = useState(null);
   const [book, setBook] = useState("fsm");
   const [chapter, setChapter] = useState("1");
+  const [breadCrumbs, setBreadCrumbs] = useState([]);
+  const [tableOfContents, setTableOfContents] = useState([]);
+
+  /* @SullivanKE: Executes once during page load to fetch the index file.
+   setIndex does not trigger updates if it is inside an async function.
+   index is then set as a promise and unpacked later.
+   I don't like this solution and I feel like it is a work around for something that has a more direct solution.
+   */
+  useEffect(() => {
+    function fetchIndexFile() {
+      const newIndex = getIndex();
+      setIndex(newIndex);
+    };
+    fetchIndexFile();
+  }, []);
+
+
+  /* @SullivanKE: Executes every time the book or chapter changes.
+  It also updates when the index is loaded.
+  Index is a promise right now I know it shouldn't be unpacked more than once.
+  This was the only way I could make the breadcrumbs render on page load, and not only when the chapter changed.
+   */
+  useEffect(() => {
+    function updateBreadcrumbs(index, book = null, unit = null) {
+      const bookNode = index.querySelector(`book[shortName='${book}']`) || index.firstElementChild;
+      const books = getBookList(index).getEntries();
+      const bookEntries = books.map(b => ({ label: b.getName(), href: b.getHref() }));
+
+      const crumbs = [
+        {
+          href: '/' + bookNode.getAttribute("shortName"),
+          label: bookNode.getAttribute("name"),
+          entries: bookEntries
+        }
+      ];
+
+      if (unit) {
+        const unitId = book + '-' + unit;
+        const unitNode = bookNode.querySelector(`[id='${unitId}']`);
+
+        crumbs.push({
+          href: '/' + book + '/' + unit,
+          label: unitNode.getAttribute("name"),
+        });
+      }
+
+      setBreadCrumbs(crumbs);
+    }
+    if (index)
+      index.then((data) => updateBreadcrumbs(data, book, chapter));
+
+    async function fetchData() {
+      let __html = await renderContent(book, chapter);
+      setHtml(__html);
+    }
+    fetchData();
+  }, [book, chapter, index]);
 
   // This should be executed just once when the page loads.
   // useEffect(function () { setHeading("Hello World!"); }, []);
-  useEffect(() => {
-    async function fetchData() {
-      let __html = await renderContent(book,chapter);
-      setHtml(__html);
-    }
 
-    fetchData();
-  }, [book,chapter]);
 
   return (
     <div id="the-app-container">
@@ -88,7 +158,7 @@ export default function App({ }) {
       {/* <Main cols='3' /> */}
       <div class="container mx-auto border-x">
         <div id="breadcrumbs" class="sticky top-0 z-5 bg-white lg:static lg:top-auto lg:z-auto lg:bg-transparent overflow-x-clip">
-          <Breadcrumbs items={[]} />
+          <Breadcrumbs items={breadCrumbs} />
         </div>
         <button
           onclick={() => {
@@ -109,7 +179,7 @@ export default function App({ }) {
               id="body"
               class="flex flex-col gap-4 subpixel-antialiased overflow-wrap break-words"
             >
-              <button style="background-color:#000;" onclick={() => {setBook("fsm"); setChapter("2");}}>Load next chapter</button>
+              <button style="background-color:#000;" onclick={() => { setBook("fsm"); setChapter("2"); }}>Load next chapter</button>
               {!html && <p>Loading...</p>}
               <div dangerouslySetInnerHTML={html}> </div>
             </div>
